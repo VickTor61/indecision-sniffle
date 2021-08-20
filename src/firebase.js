b4d9586359c52d730a55d7142ee9668f093135fc
VicTor61 <enomavictor2002@gmail.com>
2021-08-20 12:55:25 +0100

    added validation to input field

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/app";
+
+ export default firebase.initializeApp({
+    apiKey: "AIzaSyAo2SUhp6MRubYjFpDdnz1n1TL4Qco-EJY",
+    authDomain: "indecision-99c0f.firebaseapp.com",
+    projectId: "indecision-99c0f",
+    storageBucket: "indecision-99c0f.appspot.com",
+    messagingSenderId: "791982855065",
+    appId: "1:791982855065:web:b244cf64622455bbc9f908"
+  });
